b4f81ae3ae91b9d66ffd97b368d48cf849534bba
Igor Amaral <igoruftm@gmail.com>
2023-02-23 11:29:01 -0300

    finalizando css home

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 ?>
 
     <div class="container">
-
         <?php if(isset($printMsg) && $printMsg != ""): ?>
             <p id="msg"><?= $printMsg ?></p>
         <?php endif; ?>
@@ -24,14 +23,14 @@
                 <tbody>
                     <?php foreach($contacts as $contact): ?>
                         <tr>
-                            <td scope="row"><?= $contact['id'] ?></td>
+                            <td scope="row" class="col-id"><?= $contact['id'] ?></td>
                             <td scope="row"><?= $contact['name'] ?></td>
                             <td scope="row"><?= $contact['phone'] ?></td>
                             <td class="actions">
                                 <a href="#"></a>
                                 <a href="#"><i class="fas fa-eye check-icon"></i></a>
                                 <a href="#"><i class="fas fa-edit edit-icon"></i></a>
-                                <button type="submit"><i class="fas fa-times delete-icon"></i></button>
+                                <button class=" delete-btn" type="submit"><i class="fas fa-times delete-icon"></i></button>
                             </td>
                         </tr>
                     <?php endforeach; ?>
